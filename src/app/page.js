cc32bc6ef3bd4f7193c1d3aae6450bcf64ee25f8
shamsul765 <webdevlopershamsul@gmail.com>
2026-06-18 13:23:40 +0600

    use animate css service and stats section complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
 import Banner from "@/components/Banner";
+import ServicesFeature from "@/components/ServicesFeature";
+import Stats from "@/components/Stats";
+
 import Image from "next/image";
 
 export default function Home() {
+  const stats ={
+    totalCases: 300,
+    totalClients:5000,
+    totalLawyers:10000
+
+
+
+  } 
   return (
  <div>
 <Banner/>
+<ServicesFeature/>
+<Stats stats={stats}/>
  </div>
   );
 }
